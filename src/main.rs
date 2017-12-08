58f5d5c3541c01dcde66f1d7996ffe7c0876835c
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-12-08 12:34:04 -0800

    Add command for getting all unique lines in a file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ extern crate clap;
 
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashSet};
 use std::process;
 
 use clap::{App, Arg, AppSettings, SubCommand, ArgMatches};
@@ -26,9 +26,9 @@ fn cli<'a>() -> ArgMatches<'a> {
         .about("Some misc tools to help with emu dev")
         .subcommand(SubCommand::with_name("compare")
             .aliases(&["cmp", "cp"])
-            .version("1.0.0")
+            .version("1.0.1")
             .author(AUTHORS)
-            .about("Compares two files and prints the first line they are different on along with the line number")
+            .about("Compares two files and prints the first line they are different (+context lines before it) on along with the line number")
             .args(&[
                 Arg::with_name("file_1")
                     .help("The first file")
@@ -41,7 +41,29 @@ fn cli<'a>() -> ArgMatches<'a> {
                     .short("c")
                     .long("context")
                     .takes_value(true)]))
+        .subcommand(SubCommand::with_name("uniques")
+            .version("0.1.0")
+            .author(AUTHORS)
+            .about("Gets all the unique lines in a file")
+            .arg(Arg::with_name("file")
+                .help("The file to check")
+                .required(true)))
         .get_matches()
+}
+
+fn uniques(path: &String) {
+    let mut uniques = HashSet::new();
+    let file = if let Ok(f) = File::open(path) {f} else {
+        eprintln!("Failed to open file ({})", path); process::exit(1)
+    };
+
+    for line in BufReader::new(file).lines() {
+        let line = line.unwrap_or_else(|x|{eprintln!("Failed to read line! ({})", x); process::exit(1)});
+        if !uniques.contains(&line) {
+            println!("{}", line);
+            uniques.insert(line);
+        }
+    }
 }
 
 fn compare(file1: &String, file2: &String, context_len: usize) {
@@ -54,7 +76,7 @@ fn compare(file1: &String, file2: &String, context_len: usize) {
     } else {eprintln!("Failed to open file_2"); process::exit(1)};
     let mut i = 0;
     for lp in BufReader::new(f1).lines().zip(BufReader::new(f2).lines()) {
-        i = i + 1;
+        i += 1;
         let (s1, s2) = lp;
         let s1 = s1.unwrap();
         let s2 = s2.unwrap();
@@ -87,8 +109,10 @@ fn main() {
                 None => 5
             };
 
-            compare(&cmp_matches.value_of("file_1").unwrap().to_string(), &cmp_matches.value_of("file_2").unwrap().to_string(), context)
+            compare(&cmp_matches.value_of("file_1").unwrap().to_string(), &cmp_matches.value_of("file_2").unwrap().to_string(), context);
         }
-        _ => {}
+
+        ("uniques", Some(uniques_matches)) => uniques(&uniques_matches.value_of("file").unwrap().to_string()),
+        _ => unreachable!()
     }
 }
