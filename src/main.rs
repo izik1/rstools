3ac676e82a45a5dff42e20f320811e8e7c7c6192
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2018-04-01 02:44:09 -0700

    Bump dependencies + Opt (:fire:)

--- src/main.rs
+++ src/main.rs
@@ -5,72 +5,147 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+#[macro_use]
 extern crate structopt;
 
-#[macro_use]
-extern crate structopt_derive;
-
-use std::fs::File;
-use std::process;
-use std::io::{BufRead, BufReader};
-use std::collections::{HashSet, VecDeque};
-use std::error::Error;
+use std::{fmt, process, collections::{HashSet, VecDeque}, fs::File,
+          io::{self, BufRead, BufReader, Write}};
 
 use structopt::StructOpt;
+
+enum Error {
+    Io(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Io(ref e) => writeln!(f, "{}", e),
+        }
+    }
+}
+
+type Result<T> = ::std::result::Result<T, Error>;
+
+struct ReadBuffer<'a, T: BufRead + 'a> {
+    reader: &'a mut T,
+    buffer: String,
+}
+
+impl<'a, T: BufRead + 'a> ReadBuffer<'a, T> {
+    fn new(reader: &'a mut T) -> Self {
+        ReadBuffer {
+            reader,
+            buffer: String::new(),
+        }
+    }
+
+    fn get_line(&mut self) -> Result<Option<&str>> {
+        self.buffer.clear();
+        if self.reader.read_line(&mut self.buffer)? > 0 {
+            Ok(Some(self.buffer.trim()))
+        } else {
+            Ok(None)
+        }
+    }
+}
 
 #[derive(StructOpt)]
 #[structopt(about = "Some misc tools to help with emu dev")]
 enum Cli {
-    #[structopt(name = "compare",
-    aliases_raw = "&[\"cmp, cp\"]",
-    version = "1.1.0",
-    about = "Compares two files and prints the first line they are different (+context lines before it) on along with the line number")]
+    #[structopt(name = "compare", raw(aliases = r#"&["cmp, cp"]"#), version = "1.1.0",
+                about = "Compares two files and prints the first line they are different (+context lines before it) on along with the line number")]
     Compare {
         #[structopt(help = "The first file.")]
         file_1: String,
         #[structopt(help = "The second file.")]
         file_2: String,
-        #[structopt(short = "c", long = "context", help = "The number of lines to show before the diff.", default_value = "5")]
+        #[structopt(short = "c", long = "context",
+                    help = "The number of lines to show before the diff.", default_value = "5")]
         context: usize,
     },
 
-    #[structopt(name = "uniques", version = "0.2.0",
-    about = "Gets all the unique lines in a file")]
+    #[structopt(name = "uniques", version = "0.2.1",
+                about = "Gets all the unique lines in a file")]
     Uniques {
         #[structopt(help = "The file to check")]
-        file: String
-    }
+        file: String,
+    },
 }
 
-fn uniques(path: &str) -> Result<(), Box<Error>> {
+fn uniques<T: BufRead>(reader: &mut T) -> Result<()> {
+    let mut reader = ReadBuffer::new(reader);
     let mut uniques = HashSet::new();
-    for line in BufReader::new(File::open(&path)?).lines() {
-        let line = line?;
-        if !uniques.contains(&line) {
-            println!("{}", line);
-            uniques.insert(line);
+
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+
+    while let Ok(Some(line)) = reader.get_line() {
+        if !uniques.contains(line) {
+            writeln!(stdout, "{}", line)?;
+            uniques.insert(line.to_string());
         }
     }
 
     Ok(())
 }
 
-fn compare(file1: &str, file2: &str, context_len: usize) -> Result<(), Box<Error>> {
+fn compare<T: BufRead>(reader1: &mut T, reader2: &mut T, context_len: usize) -> Result<()> {
+    struct Iter<'a, 'b, T: BufRead + 'a, Q: BufRead + 'b> {
+        reader1: ReadBuffer<'a, T>,
+        reader2: ReadBuffer<'b, Q>,
+        line_num: usize,
+    }
+
+    impl<'a, 'b, T: BufRead + 'a, Q: BufRead + 'b> Iter<'a, 'b, T, Q> {
+        fn new(reader1: ReadBuffer<'a, T>, reader2: ReadBuffer<'b, Q>) -> Self {
+            Iter {
+                reader1,
+                reader2,
+                line_num: 0,
+            }
+        }
+
+        fn get_line(&mut self) -> Result<Option<(usize, &str, &str)>> {
+            // The temporary is to prevent the line number from overflowing when a reader returns `Err` or `None`.
+            let res = Ok(Some((
+                self.line_num,
+                match self.reader1.get_line()? {
+                    Some(l) => l,
+                    None => return Ok(None),
+                },
+                match self.reader2.get_line()? {
+                    Some(l) => l,
+                    None => return Ok(None),
+                },
+            )));
+
+            self.line_num += 1;
+            res
+        }
+    }
+
     let mut buf = VecDeque::with_capacity(context_len);
-    let f1 = File::open(file1)?;
-    let f2 = File::open(file2)?;
-    let mut i = 1;
-    for (s1, s2) in BufReader::new(f1).lines().zip(BufReader::new(f2).lines()) {
-        let s1 = s1?;
-        let s2 = s2?;
-        let sout = format!("{} {}", &s1, &s2);
+
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+
+    let mut iter = Iter::new(ReadBuffer::new(reader1), ReadBuffer::new(reader2));
+
+    while let Some((i, s1, s2)) = iter.get_line()? {
         if s1 != s2 {
-            println!("difference found on line {}", i);
+            writeln!(stdout, "difference found on line {}", i + 1)?;
             for s in buf {
-                println!("{}", s);
+                writeln!(stdout, "{}", s)?;
             }
 
-            println!("{}", sout);
+            writeln!(stdout, "{} {}", &s1, &s2)?;
             break;
         }
 
@@ -79,23 +154,25 @@ fn compare(file1: &str, file2: &str, context_len: usize) -> Result<(), Box<Error
                 buf.pop_front();
             }
 
-            buf.push_back(sout);
+            buf.push_back([s1, s2].join(" "));
         }
-
-        i += 1;
     }
 
     Ok(())
 }
 
-fn run_command(cli: Cli) -> Result<(), Box<Error>> {
+fn run_command(cli: Cli) -> Result<()> {
     match cli {
         Cli::Compare {
             file_1,
             file_2,
             context,
-        } => compare(&file_1, &file_2, context),
-        Cli::Uniques { file } => uniques(&file),
+        } => compare(
+            &mut BufReader::new(File::open(file_1)?),
+            &mut BufReader::new(File::open(file_2)?),
+            context,
+        ),
+        Cli::Uniques { file } => uniques(&mut BufReader::new(File::open(&file)?)),
     }
 }
 
